9f81169e456768d441e9711ec6390e9e61ed7e4c
Zordius Chen <zordius@yahoo-inc.com>
2014-09-30 17:27:28 +0800

    use request

--- api-validator.js
+++ api-validator.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports = {
-    
-};
+var lodash = require('lodash'),
+    jjv = require('jjv'),
+    REQ = require('request'),
+    ApiValidator = function (list) {
+        lodash.map(list, singleValidator);
+    },
+    loopValidator = function (list) {
+    },
+    singleValidator = function (data) {
+        
+    };
+
+module.exports = ApiValidator;
