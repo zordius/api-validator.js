e1a542f9b5e653fa54c7cba3aa47b4dc2decfe4f
Zordius Chen <zordius@yahoo-inc.com>
2014-10-04 21:19:47 +0800

    refactor for method names

--- test/test-schema.js
+++ test/test-schema.js
@@ -25,11 +25,11 @@ var assert = require('chai').assert,
         nock.cleanAll();
     };
 
-describe('Schema.findSchemaFiles', function () {
+describe('Schema.find', function () {
     it('should search for only matched pattern under the base directory', function (done) {
         assert.deepEqual([
             'test/schemas/test.json'
-        ], AS.findSchemaFiles('test/schemas', /test.json/));
+        ], AS.find('test/schemas', /test.json/));
         done();
     });
 
@@ -37,74 +37,74 @@ describe('Schema.findSchemaFiles', function () {
         assert.sameMembers([
             'test/schemas/test.json',
             'test/schemas/test2.json'
-        ], AS.findSchemaFiles('test/schemas', /test(2?).json/));
+        ], AS.find('test/schemas', /test(2?).json/));
         done();
     });
 });
 
-describe('Schema.loadSchemaFiles', function () {
+describe('Schema.load', function () {
     it('should load file content', function (done) {
         assert.deepEqual({
             'test/schemas/test.json': require('./schemas/test.json')
-        }, AS.loadSchemaFiles(['test/schemas/test.json']));
+        }, AS.load(['test/schemas/test.json']));
         done();
     });
 });
 
-describe('Schema.resolveFilePath', function () {
+describe('Schema.resolveFileRef', function () {
     it('should works with any input', function (done) {
-        assert.deepEqual(undefined, AS.resolveFilePath());
-        assert.deepEqual(1, AS.resolveFilePath(1));
+        assert.deepEqual(undefined, AS.resolveFileRef());
+        assert.deepEqual(1, AS.resolveFileRef(1));
         done();
     });
 
     it('should resolve file://abc without base', function (done) {
-        assert.deepEqual('file:///abc', AS.resolveFilePath('file:///abc'));
+        assert.deepEqual('file:///abc', AS.resolveFileRef('file:///abc'));
         done();
     });
 
     it('should resolve file://abc under /def', function (done) {
-        assert.deepEqual('file:///def/abc', AS.resolveFilePath('file://abc', '/def'));
+        assert.deepEqual('file:///def/abc', AS.resolveFileRef('file://abc', '/def'));
         done();
     });
 
     it('should resolve file://.. under /a/b/c', function (done) {
-        assert.deepEqual('file:///a/b', AS.resolveFilePath('file://..', '/a/b/c'));
+        assert.deepEqual('file:///a/b', AS.resolveFileRef('file://..', '/a/b/c'));
         done();
     });
 
     it('should resolve file://../d# under /a/b/c', function (done) {
-        assert.deepEqual('file:///a/b/d#', AS.resolveFilePath('file://../d#', '/a/b/c'));
+        assert.deepEqual('file:///a/b/d#', AS.resolveFileRef('file://../d#', '/a/b/c'));
         done();
     });
 
     it('should resolve file://../../e#2 under /a/b/c/d', function (done) {
-        assert.deepEqual('file:///a/b/e#2', AS.resolveFilePath('file://../../e#2', '/a/b/c/d'));
+        assert.deepEqual('file:///a/b/e#2', AS.resolveFileRef('file://../../e#2', '/a/b/c/d'));
         done();
     });
 });
 
-describe('Schema.resolveAllRelativePath', function () {
+describe('Schema.deepResolveFileRef', function () {
     it('should be untouched', function (done) {
-        assert.deepEqual({123: 456}, AS.resolveAllRelativePath({123: 456}));
-        assert.deepEqual({123: 456, ref: 'ok'}, AS.resolveAllRelativePath({123: 456, ref: 'ok'}));
-        assert.deepEqual({123: 456, '$ref': 'ok!'}, AS.resolveAllRelativePath({123: 456, '$ref': 'ok!'}));
-        assert.deepEqual({123: 456, '$ref': 'http://what/'}, AS.resolveAllRelativePath({123: 456, '$ref': 'http://what/'}));
+        assert.deepEqual({123: 456}, AS.deepResolveFileRef({123: 456}));
+        assert.deepEqual({123: 456, ref: 'ok'}, AS.deepResolveFileRef({123: 456, ref: 'ok'}));
+        assert.deepEqual({123: 456, '$ref': 'ok!'}, AS.deepResolveFileRef({123: 456, '$ref': 'ok!'}));
+        assert.deepEqual({123: 456, '$ref': 'http://what/'}, AS.deepResolveFileRef({123: 456, '$ref': 'http://what/'}));
         done();
     });
 
     it('should be resolved', function (done) {
-        assert.deepEqual({123: {'$ref': 'file:///a/b/c'}}, AS.resolveAllRelativePath({123: {'$ref': 'file://b/c'}}, '/a'));
+        assert.deepEqual({123: {'$ref': 'file:///a/b/c'}}, AS.deepResolveFileRef({123: {'$ref': 'file://b/c'}}, '/a'));
         done();
     });
 
     it('should resolve deeper', function (done) {
-        assert.deepEqual({123: {456: {'$ref': 'file:///a/b/c'}}}, AS.resolveAllRelativePath({123: {456: {'$ref': 'file://b/c'}}}, '/a'));
+        assert.deepEqual({123: {456: {'$ref': 'file:///a/b/c'}}}, AS.deepResolveFileRef({123: {456: {'$ref': 'file://b/c'}}}, '/a'));
         done();
     });
 
     it('should resolve with custom resolver', function (done) {
-        assert.deepEqual({123: {456: {'$ref': 'file://d/c/b/a'}}}, AS.resolveAllRelativePath({123: {456: {'$ref': 'what'}}}, 'base_dir', function (N, B) {
+        assert.deepEqual({123: {456: {'$ref': 'file://d/c/b/a'}}}, AS.deepResolveFileRef({123: {456: {'$ref': 'what'}}}, 'base_dir', function (N, B) {
             assert.deepEqual('base_dir', B);
             assert.deepEqual('what', N);
             return 'file://d/c/b/a';
