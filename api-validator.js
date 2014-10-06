8bfb5c2f3d6b484a31b45520ad396d063aa14f73
Zordius Chen <zordius@yahoo-inc.com>
2014-10-06 11:40:22 +0800

    add save and task modules, later do it

--- api-validator.js
+++ api-validator.js
@@ -5,5 +5,7 @@ module.exports = {
     schema: require('./lib/schema'),
     validate: require('./lib/validate'),
     request: require('./lib/request'),
+    save: require('./lib/save'),
+    task: require('./lib/task'),
     runner: 'todo'
 };
