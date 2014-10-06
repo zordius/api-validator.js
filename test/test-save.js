ba31c84cddc41609ffd8eb97de3f587586861893
Zordius Chen <zordius@yahoo-inc.com>
2014-10-06 15:11:07 +0800

    test on save.one

--- test/test-save.js
+++ test/test-save.js
@@ -1,7 +1,19 @@
 'use strict';
 
 var assert = require('chai').assert,
-    AS = require('../api-validator.js').save;
+    fs = require('fs'),
+    mockfs = require('mock-fs'),
+    AS = require('../api-validator.js').save,
+
+    initMockFS = function () {
+        mockfs({
+            output_dir: {}
+        });
+    },
+
+    clearMockFS = function () {
+        mockfs.restore();
+    };
 
 describe('Save.format', function () {
     it('should return number with default format', function (done) {
@@ -28,6 +40,28 @@ describe('Save.namer', function () {
         assert.equal('output/file_0004.js', AS.namer(3, null, {ext: '.js'}));
         assert.equal('output/result_0004.js', AS.namer(3, null, {ext: '.js', prefix: 'result_'}));
         assert.equal('../schemas/result_0004.js', AS.namer(3, null, {ext: '.js', prefix: 'result_', basedir: '../schemas'}));
+        done();
+    });
+});
+
+describe('Save.one', function () {
+    before(initMockFS);
+    after(clearMockFS);
+
+    it('should failed when no directory', function (done) {
+        assert.deepEqual({
+            error: [{
+                type: 'save',
+                message: "ENOENT, no such file or directory 'never/saved/directory'"
+            }]
+        }, AS.one('never/saved/directory'));
+        done();
+    });
+
+    it('should save JSON with default options', function (done) {
+        var F = 'output_dir/test.json';
+        AS.one(F, {status: 'OK'});
+        assert.equal('{\n    "status": "OK"\n}', fs.readFileSync(F, 'utf8'));
         done();
     });
 });
